0fb9d9f35b7c8359b62e686969fa5ed1c3cb31cc
= <=>
2023-02-17 09:51:05 +0100

    Diseñada estrategia local signin

--- routes/authentication.js
+++ routes/authentication.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { authenticate } = require('passport');
 const router = express.Router();
 const passport = require('passport');
 
@@ -23,8 +24,27 @@ router.post('/signup', passport.authenticate('local.signup', {
   failureFlash: true
 }));
 
+router.get('/signin', (req, res) => {
+  res.render('auth/signin')
+})
+
+router.post('/signin', (req, res, next) => {
+  passport.authenticate('local.signin', {
+    successRedirect: '/profile',
+    failureRedirect: '/signin',
+      failureFlash: true
+  })(req, res, next)
+})
+
 router.get('/profile', (req, res) =>{
-  res.send('<h1>This is your profile!</h1>');
+  res.render('profile');
 });
+
+router.get('/logout', (req, res) => {  
+  req.logOut(function(err){
+    if (err) return next(err)
+  })  
+  res.redirect('/signin')
+})
 
 module.exports = router;
